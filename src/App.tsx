f578ac37ec0200baddb4966ee752e3216fc2c979
step_65 <0345772@ukr.net>
2021-12-07 18:03:38 +0200

    added store and connected json.placegolder for 'users'

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from './hooks';
+import { fetchUsers } from './store/reducers/ActionCreators';
 
 
 function App() {
+
+  const dispatch = useAppDispatch();
+  const {users } = useAppSelector(state=> state.userReducer)
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [dispatch])
   return (
     <div className="App">
-      
+      {JSON.stringify(users, null, 2)}
     </div>
   );
 }
 
 export default App;
+
+
